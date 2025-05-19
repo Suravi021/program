a3d2d02f0ae44ab1ed85269cafe5f5b1903dea38
Suravi <suravireddy21@gmail.com>
2025-05-19 10:16:35 +0530

    Update hillclimbing.py

--- hillclimbing.py
+++ hillclimbing.py
@@ -1,28 +1,40 @@
-import random
 import matplotlib.pyplot as plt
-from math import sin, exp
+import numpy as np
 
 def f(x):
-    return 5*x**2 - 7*x + 3*sin(x) - exp(x)
+  return -x ** 2 
 
-y_val=[]
-x=0
 
-max_iter=1000
-x_val=[]
-step=1
+def hill_climbing(init_x, max_iterations = 1000, step_size = 0.9):
+  current_x = init_x
+  iteration = 0
+  current_y = f(current_x)
 
-for _ in range(max_iter):
-    x_new=x+random.randint(-step,step)/100
-    x_val.append(x_new)
-    y_val.append(f(x_new))
-    x=x_new
+  x_values = [current_x]
+  y_values = [current_y]
 
-print("global maxima:",x,"at f(x):", f(x))
+  while iteration < max_iterations:
+    neighbor_x = current_x + step_size * np.random.uniform(-1, 1)
+    neighbor_y = f(neighbor_x)
 
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.title('hill climbing')
-plt.plot(x_val,y_val)
-plt.grid(True)
+    if neighbor_y > current_y:
+      current_x = neighbor_x
+      current_y = neighbor_y
+      x_values.append(current_x)
+      y_values.append(current_y)
+
+    iteration += 1
+  return current_x, f(current_x), x_values, y_values
+
+inital_x = float(input("Enter the inital value for x: "))
+ 
+best_x, y, x_values, y_values = hill_climbing(inital_x)
+
+original_x_values = np.linspace(min(x_values), max(x_values), 1000)
+original_y_values = [f(x) for x in original_x_values]
+plt.plot(original_x_values, original_y_values)
+plt.plot(x_values, y_values)
+plt.title("Hill Climbing Algorithm")
+plt.xlabel("x")
+plt.ylabel("f(x)")
 plt.show()
